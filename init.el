b342c31eff7b65c9235eb294ae0460cf72d36925
yuji0602 <eto@cocolable.co.jp>
2015-02-09 17:47:37 +0900

    php-mode-mapの設定を削除

--- init.el
+++ init.el
@@ -18,7 +18,6 @@
          (lambda ()
              (require 'php-completion)
              (php-completion-mode t)
-             (define-key php-mode-map (kbd "C-o") 'phpcmp-complete)
              (when (require 'auto-complete nil t)
              (make-variable-buffer-local 'ac-sources)
              (add-to-list 'ac-sources 'ac-source-php-completion)
